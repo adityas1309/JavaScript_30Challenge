58d91a5d6c69bdaef2c797ba54556168c9d20b4e
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-26 23:56:14 +0530

    Create Activity4.js

---
+++ Day2/Activity4.js
@@ -0,0 +1,14 @@
+//Use && operator
+let a=true,b=false,c=true;
+console.log(a&&b); //if any of the one is false then it is false
+console.log(a&&c); //if both true then only true
+
+//Use || operatoer
+let x=true,y=false,z=false;
+console.log(x||y); //if one is true then result is true
+console.log(y||z); //if both are false then result is false
+
+//Use ! operator
+let s=true;
+console.log(s); //gives original value of s
+console.log(!s); // gives opposite of the value of s
