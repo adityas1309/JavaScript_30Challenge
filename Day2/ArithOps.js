4d0c86a94d30cf5d22386ea6c2db44aff1253922
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-27 00:15:20 +0530

    Create ArithOps.js

---
+++ Day2/ArithOps.js
@@ -0,0 +1,12 @@
+//Performs basic arithmetic operations(add,sub,mul,div,rem)
+let a=48,b=32;
+let add=a+b;
+let sub=a-b;
+let mul=a*b;
+let div=a/b;
+let rem=a%b;
+console.log(a + " + " + b + " = "+ add);
+console.log(a + " - " + b + " = "+ sub);
+console.log(a + " * " + b + " = "+ mul);
+console.log(a + " / " + b + " = "+ div);
+console.log(a + " % " + b + " = "+ rem);
