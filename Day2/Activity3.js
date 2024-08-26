efd11063015a49ec20b9a04dd1fdf04b0584e573
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-26 23:45:43 +0530

    Create Activity3.js

---
+++ Day2/Activity3.js
@@ -0,0 +1,15 @@
+//Compare two numbers using > and <
+let a=46,b=89;
+console.log(a>b);
+console.log(a<b);
+
+//Compare two numbers using >= and <=
+let c=12,d=12,e=15;
+console.log(c>=d); //if equal then true or c>d then true
+console.log(e<=d);
+
+//Compare two numbers using == and ===
+let s='12',g=12,z=12;
+console.log(s==g); //loosely checked without checking type 
+console.log(s===z); //strictly checked with checking type also
+console.log(z===g); //strictly checked with checking type also
