73db2983420f5dcfc73e4cd1773e0149f72a160e
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-27 00:25:05 +0530

    Create Comp_logical_ops.js

---
+++ Day2/Comp_logical_ops.js
@@ -0,0 +1,5 @@
+//Compare 2 nos using diff comparision operator and combines using logical ops
+let a=22,b=32;
+console.log((a<b) && (b>a));
+console.log((a<b) && (b<a));
+console.log((a<b) || (b<a));
