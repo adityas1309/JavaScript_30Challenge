b393dd01a1c0185085a02ec928d70bfc4e869586
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-26 23:28:10 +0530

    Create Activity1.js

---
+++ Day2/Activity1.js
@@ -0,0 +1,24 @@
+//WAP to add 2 nos and log result in console
+let a=20,b=50;
+add=a+b;
+console.log(add);
+
+//WAP to subtract 2 nos and log result in console
+let x=50,y=10;
+sub=x-y;
+console.log(sub);
+
+//WAP to multiply 2 nos and log result in console
+let f=88,g=32;
+mul=f*g;
+console.log(mul);
+
+//WAP to divide 2 nos and log result in console
+let d=22,e=11;
+div=d/e;
+console.log(div);
+
+//WAP to find remainder of 2 nos and log result in console
+let s=16,t=3;
+rem=s%t;
+console.log(rem);
