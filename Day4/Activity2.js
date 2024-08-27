c4eb597d77acdd6f0d3cdc86bfea1c3b239c070d
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-27 12:51:56 +0530

    Create Activity2.js

---
+++ Day4/Activity2.js
@@ -0,0 +1,14 @@
+//Sum of numbers from 1-10 using WHILE loop
+let s=0,i=1;
+while(i<=10){
+    s+=i;
+    i++;
+}
+console.log(`The sum is from 1-10 is -> ${s}`);
+
+//Print from 10-1 using while loop
+let j=10;
+while(j>0){
+    console.log(j);
+    j--;
+}
