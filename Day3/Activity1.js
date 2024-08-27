8fb58f2aa89410d19a71c1b091b373c13a6caaf8
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-27 10:40:16 +0530

    Create Activity1.js

---
+++ Day3/Activity1.js
@@ -0,0 +1,24 @@
+//Check if the number is +ve , -ve or zero using if-else
+let num=10006,a;
+if(num>0){
+    a="Positive";
+}
+else if(num<0){
+    a="Negative";
+}
+else{
+    a="Zero";
+}
+
+console.log("The number " + num + " is " + a);
+
+//Check if a person is eligible to vote(age>=18)
+let age=18,b;
+if(age>=18){
+    b="Eligible";
+}
+else{
+    b="Not Eligible";
+}
+
+console.log("The person with age " + age + " is " + b + " to give vote ");
