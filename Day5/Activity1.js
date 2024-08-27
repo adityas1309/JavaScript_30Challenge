230ed2b447a0e92c6954dc3768c1aadd41a97f45
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-27 13:54:05 +0530

    Create Activity1.js

---
+++ Day5/Activity1.js
@@ -0,0 +1,17 @@
+//function to check num is odd/even
+function check(num){
+    if(num%2==0){
+        console.log("Number is EVEN");
+    }
+    else{
+        console.log("Number is ODD");
+    }
+}
+check(17);
+
+//Funtion to calc square of a num
+function squ(num1){
+    return num1*num1;
+}
+res=squ(15);
+console.log(res);
