10411c6ffd9cb1651aabb409716d965535c976b8
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-27 12:47:45 +0530

    Create Activity1.js

---
+++ Day4/Activity1.js
@@ -0,0 +1,10 @@
+//Print numbers from 1-10 using FOR loop
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+//Print table of 5 using FOR loop
+let num=5;
+for(let i=1;i<=10;i++){
+    console.log(`${num} * ${i} = ${num*i}`);
+}
