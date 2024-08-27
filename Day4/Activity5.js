637f847ea3498ba2abd539e972a0b232d6493b93
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-27 13:44:17 +0530

    Create Activity5.js

---
+++ Day4/Activity5.js
@@ -0,0 +1,15 @@
+//Print 1-10 but skip 5 using CONTINUE
+for(let i=1;i<=10;i++){
+    if(i==5){
+        continue;
+    }
+    console.log(i);
+}
+
+//Print 1-10 but stop at 7 using BREAK
+for(let j=1;j<=10;j++){
+    if(j==7){
+        break;
+    }
+    console.log(j);
+}
