9ac44325c969e082e344d3b4817c0995d31b0464
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-27 11:04:10 +0530

    Create Activity2.js

---
+++ Day3/Activity2.js
@@ -0,0 +1,18 @@
+//Find largest number bw 3 nos using if else
+let a=14,b=19,c=18;
+if(a>=b){
+    if(a>=c){
+        console.log(a);
+    }
+    else{
+        console.log(c);
+    }
+}
+else{
+    if(b>=c){
+        console.log(b);
+    }
+    else{
+        console.log(c);
+    }
+}
