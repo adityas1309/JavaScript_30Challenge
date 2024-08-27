89a61813104ed53c6eb6641a417f79a7f807a97f
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-27 13:14:51 +0530

    Create Activity4.js

---
+++ Day4/Activity4.js
@@ -0,0 +1,15 @@
+/* PRINT THIS PATTERN USING nested loop
+*
+**
+***
+****
+*****
+*/
+let n=5;
+for(let i=1;i<=n;i++){
+    let str="";
+    for(let j=1;j<=i;j++){
+        str+="*";
+    }
+    console.log(str);
+}
