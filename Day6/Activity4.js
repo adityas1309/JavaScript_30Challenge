1491e517167df8a52ad640c800ba718efa8d62a7
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-31 21:08:02 +0530

    Create Activity4.js

---
+++ Day6/Activity4.js
@@ -0,0 +1,12 @@
+//use for loop to iterate over the array and log each element to console
+let arr1 =[1,3,5,7,9]
+for(let i=0;i<arr1.length;i++){
+    console.log(arr1[i]);
+}
+
+//use foEach to iterate over the array and log each element to console
+console.log("2nd array")
+let arr2=[2,4,6,8,10]
+arr2.forEach((element)=>{
+    console.log(element);
+})
