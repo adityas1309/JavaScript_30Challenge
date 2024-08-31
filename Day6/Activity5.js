41f4fe61d898b139c1f0bc7a576f42ae2b1f9276
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-31 21:13:45 +0530

    Create Activity5.js

---
+++ Day6/Activity5.js
@@ -0,0 +1,6 @@
+//Create a d array and log in console
+let arr= [[1,2,3],[4,5,6],[7,8,9]];
+console.log(arr);
+
+//Access and log a specific element
+console.log(`Element at (1,2) location : ${arr[1][2]}`);
