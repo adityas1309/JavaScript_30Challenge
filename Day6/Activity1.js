3ad0b80fb7cea8b9c1d94a97f1ceeb5345fd7ed9
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-31 17:11:03 +0530

    Create Activity1.js

---
+++ Day6/Activity1.js
@@ -0,0 +1,7 @@
+//Create a array of numbers from 1-5 and log the array to console
+let num = [1,2,3,4,5];
+console.log(num);
+
+//Access first and last element of the array and log them to console
+console.log(`First element is: ${num[0]}`);
+console.log(`Last element is: ${num[num.length-1]}`);
