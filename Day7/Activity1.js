fe0e0c7379da681624e4562414f455ab07b4ef47
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-31 21:21:53 +0530

    Create Activity1.js

---
+++ Day7/Activity1.js
@@ -0,0 +1,10 @@
+//Create a object book with properties and log to console
+let book = {
+    title: "Wings of Fire",
+    author: "Arun Tiwari",
+    year: 1999
+}
+console.log(book);
+
+//access and log specific properties
+console.log(`Book title is ${book.title}, and the author is ${book.author}`);
