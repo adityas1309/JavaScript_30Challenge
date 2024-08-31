4e21157c744b26895943a9b2e37e85a63ef99489
Aditya Singh <142864215+adityas1309@users.noreply.github.com>
2024-08-31 17:33:22 +0530

    Create Activity2.js

---
+++ Day6/Activity2.js
@@ -0,0 +1,14 @@
+let arr= [1,5,10,15];
+console.log("Original array:", arr);
+//Use push method to add new num in end
+arr.push(35);
+console.log("Array after push:", arr);
+//Use pop method to remove num from end
+arr.pop();
+console.log("Array after pop:", arr);
+//Use shift method to remove the 1st element
+arr.shift();
+console.log("Array after shift:", arr);
+//Use unshift method to add new num in beginning
+arr.unshift(60);
+console.log("Array after unshift:", arr);
